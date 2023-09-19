d8640ae5acacd7210bd2652a2299eb02af71d5e5
Simón Cebrero <simoncebrero95@gmail.com>
2023-09-19 00:07:46 -0300

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 const {Router } = require('express');
 const router = Router(); 
 
-router.get('/',(req, res) => {
-    res.json('Hello World')
+router.get('/test',(req, res) => {
+    const data = {
+        "name": "Fazt",
+        "website": "faztweb.com"
+    };
+    res.json(data);
 })
 
 module.exports = router;
